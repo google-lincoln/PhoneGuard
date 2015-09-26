c119e18d8c8fa4f3dda7eafbd53765d0fc1900d5
noah_lincoln <linshanying@hotmail.com>
2015-09-26 13:27:59 +0800

    android:drawable 放一个drawable资源     android:state_pressed 是否按下，如一个按钮触摸或者点击。     android:state_focused 是否取得焦点，比如用户选择了一个文本框。     android:state_hovered 光标是否悬停，通常与focused state相同，它是4.0的新特性     android:state_selected 被选中，它与focus state并不完全一样，如一个list view 被选中的时候，它里面的各个子组件可能通过方向键，被选中了。     android:state_checkable 组件是否能被check。如：RadioButton是可以被check的。     android:state_checked 被checked了，如：一个RadioButton可以被check了。     android:state_enabled 能够接受触摸或者点击事件     android:state_activated 被激活(这个麻烦举个例子，不是特明白)     android:state_window_focused 应用程序是否在前台，当有通知栏被拉下来或者一个对话框弹出的时候应用程序就不在前台了    注意:如果有多个item，那么程序将自动从上到下进行匹配，最先匹配的将得到应用。（不是通过最佳匹配）     如果一个item没有任何的状态说明，那么它将可以被任何一个状态匹配。

---
+++ src/com/diary/mobile/controller/GuideActivity.java
@@ -0,0 +1,18 @@
+package com.diary.mobile.controller;
+
+import com.diary.mobile.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GuideActivity extends Activity {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_guide);
+	}
+	
+	
+}
